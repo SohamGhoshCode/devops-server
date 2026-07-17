0a3124c79ff119483ff3f38a8a43e95bc3b39f01
Soham Ghosh <2004sohamghosh@gmail.com>
2026-07-17 13:04:05 +0530

    jen choloo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-  res.json({ message: "checking boooooooooooooooo" });
+  res.json({ message: "checking jenkins choloooo" });
 });
 
 app.listen(port, () => {
